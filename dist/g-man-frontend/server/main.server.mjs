083c295205a3cc674c2f023461301d6d0eb80df1
Sabari711 <balasabari27@gmail.com>
2024-05-07 21:22:29 +0530

    new build

--- dist/g-man-frontend/server/main.server.mjs
+++ dist/g-man-frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NLYEX6V3.mjs";import"./chunk-VWSDIWRC.mjs";import"./chunk-PP4XNAOU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-FT3A5PAC.mjs";import"./chunk-K2HX3O5E.mjs";import"./chunk-SP4AZUZB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
